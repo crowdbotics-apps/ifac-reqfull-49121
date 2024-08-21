8a2fe39139cac51c1aca8e0fcf3070d571848370
Crowdbotics <team@crowdbotics.com>
2024-08-21 11:05:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ifac-reqfull-49121",
+  url: "https://ifac-reqfull-49121.botics.co",
+  api: "https://ifac-reqfull-49121.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
